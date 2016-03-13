e5a7602f77a1b827b9ce2a2286c376482e6ae976
Vaclav Belak <vaclav.belak@merck.com>
2016-03-13 19:03:56 +0100

    Project-wide constants now as a separate trait.

---
+++ src/net/belak/vdcount/Constants.scala
@@ -0,0 +1,20 @@
+package net.belak.vdcount
+
+/**
+  * Constants used in the program.
+  * @author vaclav@belak.net
+  */
+trait Constants {
+
+  val MAX_VD_WIDTH = 50
+  val MIN_VD_WIDTH = 5
+
+  val MAX_VD_HEIGHT = 50
+  val MIN_VD_HEIGHT = 5
+
+  val MAX_VD_HW_RATIO = 1.5
+  val MIN_VD_HW_RATIO = 1.1
+
+  val INNER_PATCH_SIZE = 24 // Has to be an even number!
+
+}
