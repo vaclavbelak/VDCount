c6995774b6f5db51c4ff311a0aed0686c054da15
belak <vaclav.belak@merck.com>
2017-01-08 19:14:40 +0100

    rename

--- src/net/belak/vdcount/RunEllipseDetector.scala
+++ src/net/belak/vdcount/RunEllipseDetector.scala
@@ -8,7 +8,7 @@ object RunEllipseDetector {
   def main(args: Array[String]) {
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
     Core.setNumThreads(2)
-    val eDetector = new EllipseDetector(new File(args(0)), new File(args(1)))
+    val eDetector = new VarroaDetector(new File(args(0)), new File(args(1)))
     eDetector.convertAll()
   }
 }
